1cad29186b256e29a0765c841a057c390836507c
dauerndamprogramieren <haeusslerludwig@gmail.com>
2024-05-13 16:38:50 +0200

    framework added

---
+++ backend/src/main/java/de/dauerndambauen/projectomega/ProjectomegaGameApplication.java
@@ -0,0 +1,13 @@
+package de.dauerndambauen.projectomega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectomegaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectomegaGameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/dauerndambauen/projectomega/ProjectomegaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dauerndambauen.projectomega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectomegaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
